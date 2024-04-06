ae64269a3890924dfb980fecf24ed7f58a9b6d00
Mateus Rodrigues De Lima <70700338+limamateus@users.noreply.github.com>
2024-04-06 11:13:51 -0300

    Ajustando o nome da Controller

--- PassIn.Api/Controllers/AttendeesController.cs
+++ PassIn.Api/Controllers/AttendeesController.cs
@@ -9,7 +9,7 @@ namespace PassIn.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AttendeesController : ControllerBase
+    public class Attendees : ControllerBase
     {
 
         [HttpPost]
